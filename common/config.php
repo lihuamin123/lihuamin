fd4c01a99d6dc4f96e66977a615fcc83765b74dc
xiao li <lili@example.com>
2020-03-10 17:47:22 -0400

    diliuci he diqici xiugaile yixieyuju

--- common/config.php
+++ common/config.php
@@ -1,3 +1,5 @@
 <?php
 
   echo 'zheshi disan ci tianjia 3';
+
+  echo 'zhelishi di qi ci tijiao 7';
--- login.php
+++ login.php
@@ -4,4 +4,4 @@ echo 'zheshi diyici tijiao 1';
 
 echo 'zheshi dierci tijiao 2';
 
-
+echo 'zhelishi1 di liu ci tijiao 6';
